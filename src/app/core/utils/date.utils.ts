f97c91370d51f1f6888d73fbfcee927b8c85a03c
Andres <83299190+Andres-0304@users.noreply.github.com>
2026-09-02 18:02:31 -0500

    initial commit

--- src/app/core/utils/date.utils.ts
+++
@@ -1,7 +0,0 @@
-export function obtenerFechaFormateadaLetras(fechaObj: Date): string {
-  const meses = ['ENERO', 'FEBRERO', 'MARZO', 'ABRIL', 'MAYO', 'JUNIO', 'JULIO', 'AGOSTO', 'SEPTIEMBRE', 'OCTUBRE', 'NOVIEMBRE', 'DICIEMBRE'];
-  const dia = fechaObj.getDate();
-  const mes = meses[fechaObj.getMonth()];
-  const anio = fechaObj.getFullYear();
-  return `LIMA, ${dia} DE ${mes} DE ${anio}.`;
-}
